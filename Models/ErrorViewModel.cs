3e7c5a634d3f0fa0ecf5959dc6c80334e52a8cfb
Hoang Trung <nhtrung2312@gmail.com>
2024-11-20 18:35:16 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlowerShop_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
